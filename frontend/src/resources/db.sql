e4b3617748363ebd6524c095c23b9004086268fd
minato225 <1815131bill@gmail.com>
2022-12-23 22:56:35 +0300

    final commit

---
+++ frontend/src/resources/db.sql
@@ -0,0 +1,65 @@
+CREATE TABLE food( 
+    food_id INT(11) PRIMARY KEY AUTO_INCREMENT, 
+    food_name VARCHAR(255), 
+    food_star VARCHAR(255),
+    food_vote VARCHAR(255),
+    food_price VARCHAR(255),
+    food_discount VARCHAR(255),
+    food_desc VARCHAR(255),
+    food_status VARCHAR(255),
+    food_type VARCHAR(255),
+    food_category VARCHAR(255),
+    food_src VARCHAR(255)
+) ENGINE=INNODB;
+
+CREATE TABLE user( 
+    user_id INT(11) PRIMARY KEY AUTO_INCREMENT, 
+    user_name VARCHAR(255), 
+    user_email VARCHAR(255),
+    user_phone VARCHAR(255),
+    user_password VARCHAR(255),
+    user_birth VARCHAR(255),
+    user_gender VARCHAR(255)
+) ENGINE=INNODB;
+
+
+CREATE TABLE cart (
+  user_id INT,
+  food_id INT,
+  item_qty INT,
+  primary key (user_id, food_id)
+);
+
+CREATE TABLE booktable( 
+    book_id INT(11) PRIMARY KEY AUTO_INCREMENT, 
+    book_name VARCHAR(255), 
+    book_phone VARCHAR(255),
+    book_people INT,
+    book_tables INT,
+    user_id INT,
+    book_when VARCHAR(255),
+    book_note TEXT
+) ENGINE=INNODB;
+
+
+CREATE TABLE billdetails (
+  bill_id INT,
+  food_id INT,
+  item_qty INT,
+  primary key (bill_id, food_id)
+);
+
+CREATE TABLE billstatus (
+  bill_id INT,
+  user_id INT,
+  bill_phone VARCHAR(255),
+  bill_address TEXT,
+  bill_when VARCHAR(255),
+  bill_method VARCHAR(255),
+  bill_discount INT,
+  bill_delivery INT,
+  bill_total INT,
+  bill_paid VARCHAR(255),
+  bill_status INT,
+  primary key (bill_id)
+);
