053cdc47fbc39e5ba219bbe8a8f0fe926e5795e5
minato225 <1815131bill@gmail.com>
2022-12-24 01:47:39 +0300

    final deploy

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,6 +2,4 @@ import axios from "axios";
 
 window.axios = axios
 axios.defaults.withCredentials = false
-// axios.defaults.baseURL = "http://localhost:8081/api"
-let backendUrl = "http://" + window.location.hostname.toString() + ":8081/api"
-axios.defaults.baseURL = backendUrl
+axios.defaults.baseURL = `${process.env.VUE_APP_BASE_URL}/api`
